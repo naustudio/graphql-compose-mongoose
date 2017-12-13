adf23c03377cb0082ac24fb2e24eaadc8327e52f
nhanmaihuu <nhan.mai@naustud.io>
2017-12-13 11:13:16 +0700

    Update updateById.js

--- src/resolvers/updateById.js
+++ src/resolvers/updateById.js
@@ -108,7 +108,7 @@ export default function updateById(
           }
           if (recordData) {
             doc.set(recordData);
-            return doc.save();
+            return doc.save(resolveParams);
           }
           return doc;
         })
